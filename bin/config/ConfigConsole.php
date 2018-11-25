87f8df60722870bb01a039eb6fb537cff3e7d02f
Walderlan Sena <senawalderlan@gmail.com>
2018-11-24 23:54:08 -0300

    feature: Desenvolvido console inicial, Adicinado suporte ao worksena-router 2.0.0.

---
+++ bin/config/ConfigConsole.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: walderlan
+ * Date: 24/11/18
+ * Time: 20:30
+ */
+
+namespace Bin\Config;
+
+class ConfigConsole
+{
+    const SERVER_HOST = '0.0.0.0';
+    const SERVER_PORT = '8000';
+    const SERVER_PUBLIC_DIRECTORY = 'public/';
+}
